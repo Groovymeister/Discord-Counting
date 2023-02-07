02a1176abec440ceb6b192cb42581dcfa9cd5c3b
jrw2936 <jrw2936@g.rit.edu>
2023-02-07 00:05:15 -0500

    Timer set to 10 min

--- deposit.py
+++ deposit.py
@@ -7,7 +7,7 @@ def main():
     app = Application().connect(process=3952)
     while True:
         app.window(title = "#gambling | RIT Frens - Discord").send_keystrokes('!dep all' + '{ENTER}')
-        t.sleep(300)
+        t.sleep(600)
 
 
 if __name__ == "__main__":
