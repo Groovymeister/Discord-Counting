85334836aca2bc03470e1e832937a859ba8e07a9
jrw2936 <jrw2936@g.rit.edu>
2023-02-06 22:49:55 -0500

    Implemented counting as a background process for Discord

--- counter.py
+++ counter.py
@@ -1,13 +1,36 @@
-import keyboard
+from pynput import keyboard
+import time as t
+from pywinauto import Application
+from pywinauto.keyboard import SendKeys
 
-def count(num):
-    return None
+NUM = 0
+app = Application().connect(process=10520)
+
+def on_press_start(key):
+    if key == keyboard.Key.f10:
+        count()
+
+def on_press_loop(key):
+    if key == keyboard.Key.f9:
+        global NUM
+        NUM = int(input("Enter new number: "))
+        return False
+
+def count():
+    global NUM
+    with keyboard.Listener(on_press=on_press_loop) as listener:
+        while True:
+            NUM += 1
+            app.window(title = "#couting | RIT Frens - Discord").send_keystrokes(str(NUM) + '{ENTER}')
+            t.sleep(2)
+            if not listener.running:
+                break
 
 def main():
-    num = int(input("Please enter your starting number: "))
-    keyboard.add_hotkey('F9', exit)
-    keyboard.add_hotkey('F10', count, args = (num))
-    
+    global NUM
+    NUM = int(input("Enter last number: "))
+    with keyboard.Listener(on_press=on_press_start) as listener:
+        listener.join()
 
 
 if __name__ == "__main__":
